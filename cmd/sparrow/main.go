805d3dfb824cf0e3525138efb82b0dabcde5930d
wanglei <74483764+wanglei4687@users.noreply.github.com>
2022-05-30 01:42:31 +0800

    Add sentry (#40)
    * add sentry
    
    * add sentry
    
    * fix sentry dsn config

--- cmd/sparrow/main.go
+++ cmd/sparrow/main.go
@@ -14,7 +14,25 @@
 
 package main
 
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+)
+
 func main() {
-	println("hello world")
-	println("hello")
+	dsn := os.Getenv("SPARROW_DSN")
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: dsn,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
+
+	sentry.CaptureMessage("It works!")
+
 }
